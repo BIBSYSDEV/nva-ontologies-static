694578d72db94339008fa2aa52fd1e7a333a0b4d
brinxmat <789709+brinxmat@users.noreply.github.com>
2021-12-07 12:07:55 +0100

    Add logging

--- ontology-service/src/main/java/no/sikt/nva/Application.java
+++ ontology-service/src/main/java/no/sikt/nva/Application.java
@@ -1,10 +1,14 @@
 package no.sikt.nva;
 
 import io.micronaut.runtime.Micronaut;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 
 public class Application {
+    public static final Logger logger = LoggerFactory.getLogger(Application.class);
 
     public static void main(String[] args) {
+        logger.info("Application started");
         Micronaut.run(Application.class, args);
     }
 }
--- ontology-service/src/main/java/no/sikt/nva/OntologyController.java
+++ ontology-service/src/main/java/no/sikt/nva/OntologyController.java
@@ -2,12 +2,17 @@ package no.sikt.nva;
 import io.micronaut.http.annotation.Controller;
 import io.micronaut.http.annotation.Get;
 import io.micronaut.http.MediaType;
+import io.micronaut.http.annotation.PathVariable;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 
 @Controller
 public class OntologyController {
+    public static final Logger logger = LoggerFactory.getLogger(OntologyController.class);
 
     @Get(uri = "/{file}", produces = MediaType.APPLICATION_JSON)
-    public String getFile() {
+    public String getFile(@PathVariable("file") String file) {
+        logger.info("Get called with value: " + file);
         return "ok";
     }
 }
