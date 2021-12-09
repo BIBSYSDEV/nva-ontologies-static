b5479715f6d0ece25960f50480f9a72cdf3e42d3
brinxmat <789709+brinxmat@users.noreply.github.com>
2021-12-09 01:48:42 +0100

    Add application code

--- ontology-service/src/main/java/no/sikt/nva/OntologyController.java
+++ ontology-service/src/main/java/no/sikt/nva/OntologyController.java
@@ -1,18 +1,60 @@
 package no.sikt.nva;
+import io.micronaut.core.io.ResourceLoader;
+import io.micronaut.core.io.ResourceResolver;
+import io.micronaut.core.io.scan.ClassPathResourceLoader;
 import io.micronaut.http.annotation.Controller;
 import io.micronaut.http.annotation.Get;
-import io.micronaut.http.MediaType;
 import io.micronaut.http.annotation.PathVariable;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
+import java.io.IOException;
+import java.io.InputStream;
+import java.util.Optional;
+
 @Controller
 public class OntologyController {
     public static final Logger logger = LoggerFactory.getLogger(OntologyController.class);
+    public static final String FILE = "file";
+    public static final String APPLICATION_TURTLE = "application/turtle";
+    public static final String APPLICATION_NTRIPLES = "application/ntriples";
+    public static final String APPLICATION_RDF_XML = "application/rdf+xml";
+    public static final String APPLICATION_LD_JSON = "application/ld+json";
+    public static final String TTL = "ttl";
+    public static final String NT = "nt";
+    public static final String RDF = "rdf";
+    public static final String JSONLD = "jsonld";
+    public static final String PATH_PARAM_FILE = "{file}";
+    public static final String EXTENSION_SEPARATOR = ".";
 
-    @Get(uri = "/{file}", produces = MediaType.APPLICATION_JSON)
-    public String getFile(@PathVariable("file") String file) {
-        logger.info("Get called with value: " + file);
-        return "ok";
+    private final ResourceLoader loader = new ResourceResolver().getLoader(ClassPathResourceLoader.class).orElseThrow();
+
+    @Get(uri = PATH_PARAM_FILE, produces = APPLICATION_TURTLE)
+    public String getTurtle(@PathVariable(FILE) String file) throws IOException {
+        return getResourceAsString(file, TTL);
+    }
+
+    @Get(uri = PATH_PARAM_FILE, produces = APPLICATION_NTRIPLES)
+    public String getNTriples(@PathVariable(FILE) String file) throws IOException {
+        return getResourceAsString(file, NT);
+    }
+
+    @Get(uri = PATH_PARAM_FILE, produces = APPLICATION_RDF_XML)
+    public String getRdfXml(@PathVariable(FILE) String file) throws IOException {
+        return getResourceAsString(file, RDF);
+    }
+
+    @Get(uri = PATH_PARAM_FILE, produces = APPLICATION_LD_JSON)
+    public String getLdJson(@PathVariable(FILE) String file) throws IOException {
+        return getResourceAsString(file, JSONLD);
+    }
+
+    private String getResourceAsString(String file, String extension) throws IOException {
+        Optional<InputStream> inputStream = loader.getResourceAsStream(file + EXTENSION_SEPARATOR + extension);
+        if (inputStream.isPresent()) {
+            return new String(inputStream.get().readAllBytes());
+        }
+        logger.warn(String.format("Requested file \"%s.%s\" was not found", file, extension));
+        return null;
     }
 }
--- ontology-service/src/test/java/no/sikt/nva/OntologyControllerTest.java
+++ ontology-service/src/test/java/no/sikt/nva/OntologyControllerTest.java
@@ -5,25 +5,36 @@ import com.amazonaws.serverless.proxy.internal.testutils.MockLambdaContext;
 import com.amazonaws.serverless.proxy.model.AwsProxyRequest;
 import com.amazonaws.serverless.proxy.model.AwsProxyResponse;
 import com.amazonaws.services.lambda.runtime.Context;
+import com.fasterxml.jackson.core.JsonProcessingException;
+import com.fasterxml.jackson.databind.ObjectMapper;
 import io.micronaut.http.HttpHeaders;
 import io.micronaut.http.HttpMethod;
 import io.micronaut.http.HttpStatus;
 import io.micronaut.http.MediaType;
 import org.junit.jupiter.api.AfterAll;
-import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.Test;
 import io.micronaut.function.aws.proxy.MicronautLambdaHandler;
+import org.zalando.problem.Problem;
+import org.zalando.problem.jackson.ProblemModule;
+
+import java.net.URI;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.zalando.problem.Status.NOT_ACCEPTABLE;
 
 public class OntologyControllerTest {
 
     private static MicronautLambdaHandler handler;
     private static final Context lambdaContext = new MockLambdaContext();
+    public static ObjectMapper objectMapper;
 
     @BeforeAll
     public static void setupSpec() {
         try {
             handler = new MicronautLambdaHandler();
+            objectMapper = handler.getApplicationContext().getBean(ObjectMapper.class);
+            objectMapper.registerModule(new ProblemModule());
 
         } catch (ContainerInitializationException e) {
             e.printStackTrace();
@@ -36,11 +47,72 @@ public class OntologyControllerTest {
     }
 
     @Test
-    void shouldReturnOk() {
-        AwsProxyRequest request = new AwsProxyRequestBuilder("/file", HttpMethod.GET.toString())
-                .header(HttpHeaders.CONTENT_TYPE, MediaType.APPLICATION_JSON)
+    void shouldReturnTurtle() {
+        AwsProxyRequest request = new AwsProxyRequestBuilder("/test", HttpMethod.GET.toString())
+                .header(HttpHeaders.ACCEPT, "application/turtle")
                 .build();
         AwsProxyResponse response = handler.handleRequest(request, lambdaContext);
-        Assertions.assertEquals(HttpStatus.OK.getCode(), response.getStatusCode());
+        assertEquals(HttpStatus.OK.getCode(), response.getStatusCode());
+        assertEquals("_:b0 a \"test\" .", response.getBody());
+    }
+
+    @Test
+    void shouldReturnNTriples() {
+        AwsProxyRequest request = new AwsProxyRequestBuilder("/test", HttpMethod.GET.toString())
+                .header(HttpHeaders.ACCEPT, "application/ntriples")
+                .build();
+        AwsProxyResponse response = handler.handleRequest(request, lambdaContext);
+        assertEquals(HttpStatus.OK.getCode(), response.getStatusCode());
+        assertEquals("_:b0 <http://www.w3.org/1999/02/22-rdf-syntax-ns#type> \"test\" .",
+                response.getBody());
+    }
+
+    @Test
+    void shouldReturnNotFound() {
+        AwsProxyRequest request = new AwsProxyRequestBuilder("/not-found", HttpMethod.GET.toString())
+                .header(HttpHeaders.ACCEPT, "application/ntriples")
+                .build();
+        AwsProxyResponse response = handler.handleRequest(request, lambdaContext);
+        assertEquals(HttpStatus.NOT_FOUND.getCode(), response.getStatusCode());
+    }
+
+    @Test
+    void shouldReturnRdfXml() {
+        AwsProxyRequest request = new AwsProxyRequestBuilder("/test", HttpMethod.GET.toString())
+                .header(HttpHeaders.ACCEPT, "application/rdf+xml")
+                .build();
+        AwsProxyResponse response = handler.handleRequest(request, lambdaContext);
+        assertEquals(HttpStatus.OK.getCode(), response.getStatusCode());
+        assertEquals("<rdf:RDF xmlns:rdf=\"http://www.w3.org/1999/02/22-rdf-syntax-ns#\" />",
+                response.getBody());
+    }
+
+    @Test
+    void shouldReturnJsonLd() {
+        AwsProxyRequest request = new AwsProxyRequestBuilder("/test", HttpMethod.GET.toString())
+                .header(HttpHeaders.ACCEPT, "application/ld+json")
+                .build();
+        AwsProxyResponse response = handler.handleRequest(request, lambdaContext);
+        assertEquals(HttpStatus.OK.getCode(), response.getStatusCode());
+        assertEquals("{\"@context\": {\"@vocab\": \"https://example.org/\"}, \"name\": \"test\"}",
+                response.getBody());
+    }
+
+    @Test
+    void shouldReturnNotAcceptable() throws JsonProcessingException {
+        AwsProxyRequest request = new AwsProxyRequestBuilder("/test", HttpMethod.GET.toString())
+                .header(HttpHeaders.ACCEPT, MediaType.APPLICATION_JSON)
+                .build();
+        AwsProxyResponse response = handler.handleRequest(request, lambdaContext);
+        assertEquals(HttpStatus.NOT_ACCEPTABLE.getCode(), response.getStatusCode());
+        assertEquals("application/problem+json", getFirstMultiValueHeader(response));
+        var body = objectMapper.readValue(response.getBody(), Problem.class);
+        assertEquals(NOT_ACCEPTABLE, body.getStatus());
+        assertEquals(URI.create("about:blank"), body.getType());
+        assertEquals("Specified Accept Types [application/json] not supported. Supported types: [application/ntriples, application/ld+json, application/turtle, application/rdf+xml]", body.getDetail());
+    }
+
+    private String getFirstMultiValueHeader(AwsProxyResponse response) {
+        return response.getMultiValueHeaders().get("Content-Type").get(0);
     }
 }
